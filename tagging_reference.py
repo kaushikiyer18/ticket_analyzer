f938179f2f868232c7683f1d809214ce044dcb65
kaushikiyer18 <135979736+kaushikiyer18@users.noreply.github.com>
2025-05-06 17:09:18 +0530

    Create tagging_reference.py

---
+++ tagging_reference.py
@@ -0,0 +1,35 @@
+# tagging_reference.py
+
+# Ticket Types (all must start with "CEE -")
+TICKET_TYPE_REFERENCE = [
+    "CEE - API issue",
+    "CEE - Campaign issue",
+    "CEE - Customer queries",
+    "CEE - Database uploading issue",
+    "CEE - Event not reflecting",
+    "CEE - Journey issue",
+    "CEE - Non Relevant",
+    "CEE - Reports issue",
+    "CEE - Segment issue",
+    "CEE - SFTP issue",
+    "CEE - Spam issues",
+    "CEE - Task",
+    "CEE - Template issue",
+    "CEE - UI Functional issues/bugs",
+    "CEE - Webhooks issue"
+]
+
+# Issue Types
+ISSUE_TYPE_REFERENCE = [
+    "Configuration",
+    "Enhancement",
+    "Global incident",
+    "Integration",
+    "Knowledge",
+    "Monitoring",
+    "No Concern",
+    "Query",
+    "One-time incident",
+    "Task",
+    "Tech"
+]
